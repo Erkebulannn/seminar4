d315b553c9732e80d1e630bce429770f74538757
Erkebulannn <92359908+Erkebulannn@users.noreply.github.com>
2021-10-23 10:34:40 +0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,13 +1,9 @@
-try:
-        a =int(input("1-shi san:"))
-        b =int(input("2-shi san:"))
-        c =int(input("3-shi san:"))
-except:
-        print("Error")
-        exit()
+a =int(input("1-shi san:"))
+b =int(input("2-shi san:"))
+c =int(input("3-shi san:"))
 
 def my_func(n1,n2,n3):
-        if((a==b)or(a==c)or(b==c)):
+        if((n1==n2)or(n1==n3)or(n2==n3)):
                 print('yes')
         else:
                 print('no')
